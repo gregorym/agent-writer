cbe57210824badcca30ed6179041b34f8388e3f6
Grégory Marcilhacy <g.marcilhacy@gmail.com>
2025-05-14 13:23:07 -0700

    Add sitemap

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,29 @@
+import { getSortedPostsData } from "@/lib/blog";
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const posts = getSortedPostsData();
+
+  const blogs = posts.map((post) => ({
+    url: `https://agentwriter.co/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+    changeFrequency: "weekly" as const,
+    priority: 0.7,
+  }));
+
+  return [
+    {
+      url: "https://agentwriter.co",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://agentwriter.com/blog",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    ...blogs,
+  ];
+}
